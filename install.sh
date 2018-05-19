8dbde0a8a55fce2898588d4bda0b6bd25c90ad81
mattHawthorn <hawthorn.matthew@gmail.com>
2018-05-19 14:49:42 -0400

    more readable output in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
+LN_FLAGS="$@"
+
 prompt_symlink() { 
     local a
-    printf "symlink $1?"
-    read a
+    read -p "symlink $1? " a
     case "$a" in 
-        y|Y) ln -s "$1" && echo "$1 symlinked";; 
+        y|Y) ln $LN_FLAGS -s "$1" && 
+                echo $'\n'"    $1 successfully symlinked"$'\n' || 
+                echo $'\n'"    Error symlinking $1!"$'\n' 
+        ;; 
     esac;
 }
 
@@ -14,7 +18,7 @@ TMPFILE="$(mktemp)"
 find $DOTFILESDIR -maxdepth 1 -name '.*' -not -name '.gitignore' -not -name '.git' -exec echo {} \; > $TMPFILE
 echo "$DOTFILESDIR/scripts" >> $TMPFILE
 
-for line in $(cat $TMPFILE); do prompt_symlink "$line"; done;
+for line in $(cat $TMPFILE | sort); do prompt_symlink "$line"; done;
 rm $TMPFILE
 
 [ -f .bash_profile ] && source .bash_profile
