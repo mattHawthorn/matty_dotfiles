1b786ae82eed446f2d95b8dba4d5d7338867375f
Matthew Hawthorn <hawthorn.matthew@gmail.com>
2019-01-04 00:38:40 -0500

    added recursive symlinking capability to installation script, cleaned up .gitignores to reflect, and moved unwanted files out of repo directory

---
+++ .ipython/profile_default/startup/00-notebook_persist.py
@@ -0,0 +1,5 @@
+try:
+    from spgmi_utils.ioutils import persist_notebook_session, restore_notebook_session
+except ImportError:
+    pass
+
--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
+if [ "$1" = '-h' ] || [ "$1" = "--help" ]; then
+    echo
+    echo "Usage: ./install.sh [-x|--no-prompt] [--dir TARGET_DIR] [FILE1 [FILE2 [...]]]"
+    echo
+    echo "Install custom dotfiles into your home directory or alternately a custom directory."
+    echo "Most files are symlinked directly but for some, whose parent apps have deeply nested "
+    echo "configuration directories, a subset of custom configuration is copied into those "
+    echo "corresponding directories in the target recursively."
+    echo "To specify only a subset of the dotfiles, pass the desired files as trailing positional args."
+    exit 0
+fi
+
 if [ "$1" = '-x' ] || [ "$1" = "--no-prompt" ]; then
-    CMD=symlink; shift
+    CMD=symlink; RCMD=rsymlink; shift
     echo "Symlinking files without prompts"
 else
-    CMD=prompt_symlink
+    CMD=prompt_symlink; RCMD=prompt_rsymlink
 fi
 
 if [ "$1" = "--dir" ]; then
@@ -17,6 +29,10 @@ fi
 
 echo "Installing symlinks in $DIR"
 
+fullpath() {
+    cd "$1" >/dev/null; pwd; cd - >/dev/null
+}
+
 LN_FLAGS=()
 while [ "${1#-}" != "$1" ]; do
     LN_FLAGS=("${LN_FLAGS[@]}" "$1"); shift
@@ -26,14 +42,29 @@ LN_FLAGS=("${LN_FLAGS[@]}" "-s")
 echo "Passing flags ${LN_FLAGS[@]} to ln"
 
 FILES=("$@")
+RECURSE_DIRS=(".ipython" ".atom")
+EXTRAS=("scripts")
+IGNORE=(".gitignore" ".git" ".DS_Store" ".idea")
 
-DOTFILESDIR="$(dirname $BASH_SOURCE)"
+DOTFILESDIR="$(fullpath $(dirname $BASH_SOURCE))"
 TMPFILE="$(mktemp)"
+
 
 if [ ${#FILES[@]} -eq 0 ]; then
     echo "Finding relevant config files in $DOTFILESDIR"
-    find $DOTFILESDIR -maxdepth 1 -name '.*' -not -name '.gitignore' -not -name '.git' -exec echo {} \; > $TMPFILE
-    echo "$DOTFILESDIR/scripts" >> $TMPFILE
+    echo
+    
+    cmd="find $DOTFILESDIR -maxdepth 1 -name '.*'"
+    # exclude dirs which will be copied
+    
+    for file in ${RECURSE_DIRS[@]} ${IGNORE[@]}; do
+        cmd="$cmd -not -name '$file'"
+    done
+    eval "$cmd -exec echo {} \\;" > $TMPFILE
+    
+    for file in ${EXTRAS[@]}; do
+        echo "$DOTFILESDIR/$file" >> $TMPFILE
+    done
 else
     for FILE in "${FILES[@]}"; do
         echo "$DOTFILESDIR/$FILE" >> $TMPFILE
@@ -48,7 +79,7 @@ fi
 
 prompt_symlink() { 
     local a
-    read -p "symlink $1? " a
+    read -p "symlink $(basename $1)? " a
     case "$a" in 
         y|Y) symlink "$1" 
         ;; 
@@ -61,8 +92,35 @@ symlink() {
                 echo $'\n'"    Error symlinking $1 in $DIR!"$'\n'
 }
 
+rsymlink() {
+    local line path
+    find "$1" -type d | while read line; do
+        path=${line#$DOTFILESDIR}
+        ! [ -d "$DIR/$path" ] && mkdir "$DIR/$path"        
+    done
+    find "$1" -type f | while read line; do
+        path=${line#$DOTFILESDIR}
+        if [ -f "$DIR/$path" ]; then
+            echo "$DIR/$path already exists! refusing to overwrite"
+        else
+            ln ${LN_FLAGS[@]} "$DOTFILESDIR/$path" "$DIR/$path" 
+        fi    
+    done
+}
+
+prompt_rsymlink() { 
+    local a
+    read -p "recursively symlink $1 contents into $DIR/$1? " a
+    case "$a" in 
+        y|Y) rsymlink "$1" 
+        ;; 
+    esac;
+}
+
 for line in $(cat $TMPFILE | sort); do "$CMD" "$line"; done;
 rm $TMPFILE
+
+for line in ${RECURSE_DIRS[@]}; do "$RCMD" "$line"; done;
 
 [ -e .bash_profile ] && echo $'\n'"Run 'source ~/.bash_profile' to take advantage of your new shell configuration"$'\n'
 echo Finished!
