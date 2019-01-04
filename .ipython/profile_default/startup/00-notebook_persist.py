b3e06dec8de9e9a8216c71cbd5816c3027ed7d1d
Matthew Hawthorn <hawthorn.matthew@gmail.com>
2019-01-04 01:30:22 -0500

    added load_global import in .ipython/profile_default/startup/00-notebook_persist.py

--- .ipython/profile_default/startup/00-notebook_persist.py
+++ .ipython/profile_default/startup/00-notebook_persist.py
@@ -1,5 +1,5 @@
 try:
-    from spgmi_utils.ioutils.notebook_state import persist_notebook_session, restore_notebook_session, load_global
+    from spgmi_utils.ioutils.notebook_state import persist_notebook_session, restore_notebook_session, save_global, load_global
 except ImportError:
     pass
 
