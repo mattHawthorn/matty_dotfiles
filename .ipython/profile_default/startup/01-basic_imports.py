d4ba4694b548fb925eb2bd76e6720b64a5c81807
Matthew Hawthorn <hawthorn.matthew@gmail.com>
2019-01-05 12:17:17 -0500

    added standard library and commonly used package imports script in .ipython/profile_default/startup

---
+++ .ipython/profile_default/startup/01-basic_imports.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import re
+import json
+import pickle
+import pathlib
+import logging
+import functools
+import itertools
+import operator
+import multiprocessing as mp
+import requests
+import tqdm
